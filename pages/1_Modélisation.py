50c81fa946f3189422af782020b79217f5e2e06c
Ludovic Mailleret <lmailleret@gmail.com>
2022-09-19 15:59:24 +0200

    en tete des pages, ajout image

--- pages/1_Modélisation.py
+++ pages/1_Modélisation.py
@@ -3,3 +3,14 @@ import streamlit as st
 
 from utils.funTISapp import *
 
+st.set_page_config(layout="wide", page_title = "Modélisation", page_icon = "🌳")
+
+st.sidebar.header("Modélisation")
+
+col2, col3 = st.columns([5, 15], gap = "large")
+
+with col2:
+    st.image("img/ceratitis.png", width=250)
+with col3:
+    st.markdown("$~$")
+    st.markdown("# Technique de l'Insecte Stérile et point de basculement")
--- pages/2_Simulations.py
+++ pages/2_Simulations.py
@@ -3,3 +3,14 @@ import streamlit as st
 
 from utils.funTISapp import *
 
+st.set_page_config(layout="wide", page_title = "Modélisation", page_icon = "🌳")
+
+st.sidebar.header("Simulations")
+
+col2, col3 = st.columns([5, 15], gap = "large")
+
+with col2:
+    st.image("img/ceratitis.png", width=250)
+with col3:
+    st.markdown("$~$")
+    st.markdown("# Technique de l'Insecte Stérile et point de basculement")
