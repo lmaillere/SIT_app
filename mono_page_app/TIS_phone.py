4a6cce23d24c9e89097778431dbb37c502d2b1df
Ludovic Mailleret <lmailleret@gmail.com>
2022-09-30 11:40:04 +0200

    update dire name

--- mono_page_app/TIS_phone.py
+++ mono_page_app/TIS_phone.py
@@ -10,3 +10,5 @@ col1, col2, col3 = st.columns([0.5, 5, 1])
 with col2:
     st.image("https://forgemia.inra.fr/ludovic.mailleret/figures/-/raw/master/ceratitis_capitata/ceratitis_small.png", width = 400)
     st.markdown("## Technique de l'Insecte Stérile et points de bascule")
+
+    
